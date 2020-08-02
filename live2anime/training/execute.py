6fed5ccdc681a4f898f46cfb632fb951a75418f5
Leonardo Torin <leonardo_torin@yahoo.com.br>
2020-08-02 18:14:11 -0300

    adding configuration for gpu support

--- live2anime/training/execute.py
+++ live2anime/training/execute.py
@@ -1,8 +1,18 @@
+import tensorflow as tf
 from live2anime.models.discriminator import define_discriminator
 from live2anime.models.generator import define_generator
 from live2anime.models.gan import define_gan
 from live2anime.training.train import train
 from live2anime.io.load import load_real_samples
+
+#config for using gpu on tensorflow...https://github.com/tensorflow/tensorflow/issues/24496
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+	try:
+		for gpu in gpus:
+			tf.config.experimental.set_memory_growth(gpu, True)
+	except RuntimeError as e:
+		print(e)
 
 # load image data
 dataset = load_real_samples('data/npz_files/pictures.npz')
